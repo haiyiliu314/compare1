e895ef38c01e407ed5ef9d8160d9f156f42b036a
haiyiliu314 <liuhai@umich.edu>
2018-07-06 15:42:46 -0400

    Add files via upload

--- arguments.f90
+++ arguments.f90
@@ -8,10 +8,10 @@
   !t_end: whole time
 !  integer                                      ::m(2*Nm_o+1) = 0
 !  !m: order of circular harmonics
-  double precision, parameter                  ::ymax = 35.0d0, dy = ymax/dble(Ny) 
+  double precision, parameter                  ::ymax = 10.0d0, dy = ymax/dble(Ny) 
   !ymax: maximum value for y   dy: length of one step for y grid
-  double precision, parameter                  ::sigmat = 0.3d0, tstart = -1d0,&
-                                                 sigmat_A = 0.2d0, tstart_A = -1d0  
+  double precision, parameter                  ::sigmat = 0.3d0, tstart = -3d0,&
+                                                 sigmat_A = 0.02d0, tstart_A = -1d0  
 !                                                 sigmat_A = 0.2d0*scale1, tstart_A = -0.5d0*scale1  
   !sigmat: sigma for gaussian pulse(E(t) = exp(-t^2/sigmat^2))(ps)   tstart: starting point for
   !time (ps) 
@@ -22,8 +22,8 @@
   double precision, parameter                  ::Ebind = 4.18d0, gamma = 1.56d0, Eg = 1.50d3
   !Ebind: binding energy(meV)   gamma: dephasing factor (meV) Eg: ground state energy
   double precision, parameter                  ::omega_1s = (0d0)/hbar, &    
-                                                 A_freq_para = (Eg)/2d0/hbar
-!                                                 A_freq_para = (4d0-4d0/9d0)*Ebind/hbar
+                                                 !A_freq_para = (Eg-4d0*Ebind+2d0*Ebind+12d0*Ebind*(20d0/50d0 - 0.5d0))/2d0/hbar
+                                                 A_freq_para = (Eg-(4d0/9d0)*Ebind)/2d0/hbar
 !                                                 A_freq_para = omega_1s
 !                                                dipole: unit: m*e(elementary charge)
   double precision, parameter                  ::dt = t_end/dble(Nt)           
@@ -42,10 +42,11 @@
                                                  W2( Ny ), A2( LDA, Ny )
   character                                    ::YES = 'V',NO = 'N'
 !-------------------end LAPACK-----------------------
-  double precision                             ::E_excit = 1.0d-3, shift = (Eg - omega_1s*hbar)/Ebind,A_excit = 1d6*(Eg/3d0/hbar)/A_freq_para
+  double precision                             ::E_excit = 1.0d-3, shift = (Eg - omega_1s*hbar)/Ebind,A_excit = 1d5*(Eg/3d0/hbar)/A_freq_para, gamma_m = 1d0
   !E_excit: excitation level(unit: binding energy)  shift: shift caused by rotation frame 
   !(unit: binding energy)
   !A_excit: unit: V*ps/m
+  !gamma_m: dephasing caused by quantum memory unit: meV
   double precision                             ::coul_mat(Nm_o+1, Ny, Ny) = 0.0d0, &
                                                  Etemp(Ny), Etemp1(Ny), Emax, dipole(Ny) = 5d-10
   !coul_mat: Coulomb matrix(non-symmetric)(unit: binding energy) Et: electrical field 
@@ -56,24 +57,26 @@
   !y_fine: finer grid for removal of singularity   dy_fine: length of one step of finer grid for
   !removal of ringularity  f:density
   complex*16                                   ::p(2*Nm_o+1, Ny) = 0.0d0, ii = (0.0d0, 1.0d0), &
-                                                 f(2*Nm_o+1, Ny) = 0.0d0, coup(Ny),&
-                                                 p_proj(Nt_RWA, Ny), p_proj1(Nt_RWA, Ny), &
-                                                 p_proj2(Nt_RWA, Ny), p_proj11(Nt_RWA, Ny), &
-                                                 p_proj22(Nt_RWA, Ny)                
+                                                 f(2*Nm_o+1, Ny) = 0.0d0, coup(Ny), &
+                                                 decay(2*Nm_o+1, Ny) = 0.0d0, &
+                                                 p_proj(Ny), p_proj1(Ny), &
+                                                 p_proj2(Ny), p_proj11(Ny), &
+                                                 p_proj22(Ny)                
   !p: polarization   f: density		coup: magnetism coupling term
   complex*16                                   ::pt(2*Nm_o+1) = 0.0d0, &
                                                  ft(2*Nm_o+1) = 0.0d0, &
                                                  J_THZ_t = 0.0d0                 
   !pt: macroscopic polarization as a function of time ft: macroscopic density as a function of time
   complex*16                                   ::p_freq(N_freq) = 0.0d0, E_freq(N_freq) = 0.0d0,&
-                                                 A_freq(N_freq) = 0.0d0, J_THZ_freq(N_freq) = 0.0d0   
+                                                 A_freq(N_freq) = 0.0d0, J_THZ_freq(N_freq) = 0.0d0, &
+                                                 p1_freq(N_freq) = 0.0d0   
   !p_freq: Fourier transform of polarization   E_freq: Fourier transform of electrical field
   double precision                             ::freqgrid(N_freq) = 0.0d0, test(Nphi) = 0.0d0     
   !freqgrid: frequency grid used for Fourier transform   test: irrelevant with code
   character(80)                                ::list_file, list_file1, list_file2, list_file3, &
                                                  list_file4, list_file5
   !list_file: file name for output
-  character(len=100)                           ::format_V, format_V1, format_V2, format_V3     
+  character(len=100)                           ::format_V, format_V1, format_V2, format_V3, format_V4     
   !format_V: format for output
   contains
 
