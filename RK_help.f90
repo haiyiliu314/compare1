b7fa4fc89aef100ae4574b6158462706bb171995
haiyiliu314 <liuhai@umich.edu>
2018-11-15 08:25:34 -0500

    Add files via upload

--- RK_help.f90
+++ RK_help.f90
@@ -17,20 +17,19 @@
   !format_V: viarable for output format
   !--------set frequence grid, from 1 E_B to 9 E_B ---
   do i1 = 1,N_freq
-    freqgrid(i1) =0.001d0*i1-0.0005d0-0.5
+    freqgrid(i1) =1d0/N_freq*(i1 - 0.5d0)-0.5d0
 !    freqgrid(i1) =1d0/N_freq*i1-0.5d0/N_freq-0.5d0
 !    freqgrid(i1) =1d0+0.01d0*i1-0.005d0
   end do
 !-----------output the frequency grid---------------
-  freqgrid = (1d0*Eg+freqgrid*0.2d0*Eg)/hbar
-!  freqgrid = (3d0*Eg+freqgrid*6d0*Eg)/hbar
-!  freqgrid = (freqgrid*Ebind)/hbar
+  freqgrid = (0.5d0*Eg+freqgrid*0.15d0*Eg)/hbar
+!  freqgrid = (Eg - 4d0*Ebind +freqgrid*6d0*Ebind)/hbar
+!  freqgrid = (1.5d0*Eg+freqgrid*3d0*Eg)/hbar
   write(list_file, '(A)') 'freqgrid.dat'           !p(t)
   open(unit=700,file=list_file)
   write(format_V, '(A12, I6, A18)')   '(SE24.16e3, ', N_freq, '(", ",SE24.16e3))'
   do i1 = 1, N_freq
     write(700, format_V) freqgrid(i1)*hbar/Eg
-!    write(700, format_V) freqgrid(i1)
   end do
   close(700)
 !------------end output-----------------------------
@@ -43,9 +42,9 @@
     double precision                             ::t1
     t1 = tvia*dt+tstart_A                         
     !tvia: time step(converted into double precision)
-    Etime = A_excit*1d3*exp(-t1*t1/(sigmat_A*sigmat_A)) * (A_freq_para*(exp(ii*delta_1s*t1)*exp(ii*2d0*omega_1s*t1)-exp(-ii*delta_1s*t1))/(2d0*ii) + 2d0*t1/(sigmat_A*sigmat_A) * (exp(-ii*delta_1s*t1) + exp(ii*delta_1s*t1)*exp(ii*2d0*omega_1s*t1))/2d0)!meV non RWA
+    Etime = A_excit*1.0d3*exp(-t1*t1/(sigmat_A*sigmat_A)) * (A_freq_para*(exp(ii*delta_1s*t1)*exp(ii*2.0d0*omega_1s*t1)-exp(-ii*delta_1s*t1))/(2.0d0*ii) + 2.0d0*t1/(sigmat_A*sigmat_A) * (exp(-ii*delta_1s*t1) + exp(ii*delta_1s*t1)*exp(ii*2.0d0*omega_1s*t1))/2.0d0)!meV non RWA, 1d3 for eV to meV 
 !    Etime = A_excit*Ebind*exp(-t1*t1/(sigmat_A*sigmat_A)) * (A_freq_para*sin(A_freq_para*t1))/exp(ii*omega_1s*t1)!no Ebind non RWA just sinusoidal part
-!    Etime = E_excit*exp(-(t1)*(t1)/(sigmat*sigmat)) * (1d0 + exp(-ii*2d0*omega_1s*t1))!no Ebind   RWA
+!    Etime = E_excit*exp(-t1*t1/(sigmat*sigmat)) * (1d0 + exp(-ii*2.0d0*omega_1s*t1))!no Ebind   RWA
   end function Etime
 
   complex*16 function Atime(tvia)
@@ -55,14 +54,14 @@
     double precision                             ::t1
     t1 = tvia*dt+tstart_A                     
     !tvia: time step(converted into double precision)
-!    Atime = A_excit*exp(-((tvia)*dt+tstart_A)*((tvia)*dt+tstart_A)/(sigmat_A*sigmat_A))*cos(A_freq_para*t1/hbar)*Ebind*2d0
+!    Atime = A_excit*exp(-((tvia)*dt+tstart_A)*((tvia)*dt+tstart_A)/(sigmat_A*sigmat_A))*cos(A_freq_para*t1/hbar)*Ebind*2.0d0
 !    Atime = A_excit*exp(-((tvia)*dt+tstart_A)*((tvia)*dt+tstart_A)/(sigmat_A*sigmat_A))*(exp(ii*A_freq_para*t1/hbar)+exp(-ii*A_freq_para*t1/hbar))*Ebind        ! unit:meV    
     Atime = A_excit*exp(-t1*t1/(sigmat_A*sigmat_A))*(cos(A_freq_para*t1))        ! unit:V*ps/m                    
   end function Atime
 
 
-  subroutine RHS(nt_via, f_via, p_via, &
-                 p_out, f_out, kE_out, kPfreq_out, kA_out, kJ_out)
+  subroutine RHS(nt_via, f_via, p_via, decay_via, &
+                 p_out, f_out, decay_out, kE_out, kPfreq_out, kP1freq_out, kA_out, kJ_out)
   !08/25/2017 creation
   !calculate the right hand side of RK
     double precision, intent(in)                           ::nt_via   
@@ -71,12 +70,15 @@
     !polarization of last time step
     complex*16, intent(in)                                 ::f_via(2*Nm_o+1, Ny)   
     !density of last time step
+    complex*16, intent(in)                                 ::decay_via(2*Nm_o+1, Ny)   
     complex*16, intent(out)                                ::p_out(2*Nm_o+1, Ny)  
     !polarization output
     complex*16, intent(out)                                ::f_out(2*Nm_o+1, Ny) 
     !density output
+    complex*16, intent(out)                                ::decay_out(2*Nm_o+1, Ny) 
     complex*16, intent(out)                                ::kE_out(N_freq)
     complex*16, intent(out)                                ::kPfreq_out(N_freq)
+    complex*16, intent(out)                                ::kP1freq_out(N_freq)
     complex*16, intent(out)                                ::kA_out(N_freq)
     complex*16, intent(out)                                ::kJ_out(N_freq)
     !kE_out, kPfreq_out, kA_out, kJ_out: output parameters for Runge-Kutta methods
@@ -88,7 +90,12 @@
                                                              pp_sum_plus(Ny), &
                                                              pf_sum(Ny), &
                                                              fp_sum(Ny), &
-                                                             p_sum_part_m(Ny)
+                                                             p_sum_part_m(Ny), &
+                                                             decayf_sum(Ny), &
+                                                             fdecay_sum(Ny), &
+                                                             decay_sum_part(Ny), &
+                                                             decay_sum_part_m(Ny)
+    double precision                                       ::t1
     !help parameters to compute all the summations
     !p_sum_part: \Sigma V_{k'-k}P_{k'}
     !f_sum_part: \Sigma V_{k'-k}f_{k'}
@@ -111,47 +118,68 @@
       fp_sum = 0.0d0
       p_sum_part_m = 0.0d0
       f_sum_part = 0.0d0
+      decayf_sum = 0.0d0
+      fdecay_sum = 0.0d0
+      decay_sum_part = 0.0d0
+      decay_sum_part_m = 0.0d0
       do Ndo_m1 = 1, 2*Nm_o+1
 
         f_sum_part = matmul(f_via(Ndo_m1, :), coul_mat(abs(Ndo_m1-Nm_o-1)+1, :, :))                                                                               
-        p_sum_part = matmul(p_via(Ndo_m1, :), coul_mat(abs(Ndo_m1-Nm_o-1)+1, :, :))                       
+        p_sum_part = matmul(p_via(Ndo_m1, :), coul_mat(abs(Ndo_m1-Nm_o-1)+1, :, :))
+        decay_sum_part = matmul(decay_via(Ndo_m1, :), coul_mat(abs(Ndo_m1-Nm_o-1)+1, :, :))                        
         if(abs(Ndo_m-Ndo_m1)<Nm_o+1) then
           fp_sum = fp_sum+f_via(Ndo_m-Ndo_m1+(Nm_o+1), :)*p_sum_part
           pf_sum = pf_sum+p_via(Ndo_m-Ndo_m1+(Nm_o+1), :)*f_sum_part
+          fdecay_sum = fdecay_sum+f_via(Ndo_m-Ndo_m1+(Nm_o+1), :)*decay_sum_part
+          decayf_sum = decayf_sum+decay_via(Ndo_m-Ndo_m1+(Nm_o+1), :)*f_sum_part
           pp_sum = pp_sum+conjg(p_via(-Ndo_m+Ndo_m1+(Nm_o+1), :))*p_sum_part
         end if
         if(abs(Ndo_m+Ndo_m1 - 2*(Nm_o+1))<Nm_o+1) then
           pp_sum_plus = pp_sum_plus+p_via(Ndo_m+Ndo_m1-(Nm_o+1), :)*conjg(p_sum_part)
         end if
-!        write(700, format_V) aimag(conjg(p_via(-Ndo_m+Ndo_m1+(Nm_o+1), :))*p_sum_part)
       end do
 
       !magnetism coupling, P^{m-1}+P^{m+1}, and truncate at Nm_o, -Nm_o
       if(abs(Ndo_m-Nm_o-1)<Nm_o) then
-        coup = (p_via(Ndo_m-1, :)+p_via(Ndo_m+1, :))/2d0
+        coup = (p_via(Ndo_m-1, :)+p_via(Ndo_m+1, :))/2.0d0
         else if(Ndo_m == 1) then
-        coup = (p_via(Ndo_m+1, :))/2d0
+        coup = (p_via(Ndo_m+1, :))/2.0d0
         else if(Ndo_m == 2*Nm_o+1) then
-        coup = (p_via(Ndo_m-1, :))/2d0
+        coup = (p_via(Ndo_m-1, :))/2.0d0
       end if
 !      coup = 0d0
       p_sum_part_m = matmul(p_via(Ndo_m, :), coul_mat(abs(Ndo_m-Nm_o-1)+1, :, :))
-      p_out(Ndo_m, :) = -(0.0d0,1.0d0)*(y*y*p_via(Ndo_m, :) - 2.0d0*pf_sum - 250d0/hbar*1d-7*y*Atime(nt_via)*coup+&
-                        shift*p_via(Ndo_m, :) - (0.0d0,1.0d0) * gamma * p_via(Ndo_m, :)/Ebind -&
-                        ((abs(dble(Ndo_m == (Nm_o+1)))-2.0d0*f_via(Ndo_m, :)) *Etime(nt_via)*dipole/Ebind+&
-                        (p_sum_part_m - 2.0d0*fp_sum) ) )/hbar*dt*Ebind
+      p_out(Ndo_m, :) = -ii*(y*y*p_via(Ndo_m, :) - 2.0d0*pf_sum - 250.0d0/hbar*1d-7*y*Atime(nt_via)*coup&
+                        +shift*p_via(Ndo_m, :)&
+                        -((abs(dble(Ndo_m == (Nm_o+1)))-2.0d0*f_via(Ndo_m, :)) *Etime(nt_via)*dipole/Ebind+&
+                       (p_sum_part_m - 2.0d0*fp_sum) )- ii*decay_m*decay_via(Ndo_m, :) )/hbar*dt*Ebind
+!      p_out(Ndo_m, :) = -(0.0d0,1.0d0)*(y*y*p_via(Ndo_m, :) - 2.0d0*pf_sum - 250d0/hbar*1d-7*y*Atime(nt_via)*coup+&
+!                        shift*p_via(Ndo_m, :) - (0.0d0,1.0d0) * gamma * p_via(Ndo_m, :) -&
+!                        ((abs(dble(Ndo_m == (Nm_o+1)))-2.0d0*f_via(Ndo_m, :)) *Etime(nt_via)*dipole/Ebind+&
+!                        (p_sum_part_m - 2.0d0*fp_sum) ) )/hbar*dt*Ebind
       f_out(Ndo_m, :) = (conjg(Etime(nt_via)*dipole/Ebind)*p_via(Ndo_m, :) - Etime(nt_via)*dipole/Ebind&
                         *conjg(p_via((2*Nm_o+2)-Ndo_m, :)) &
-                        +(pp_sum_plus-pp_sum)- (0.0d0,1.0d0) *(dble(Ndo_m==Nm_o+1)+1)*gamma /Ebind&
+                        +(pp_sum_plus-pp_sum)- ii *(dble(Ndo_m==Nm_o+1)+1.0d0)*gamma &
                         * f_via(Ndo_m, :))&
-                        /hbar*dt/(0.0d0, 1.0d0)*Ebind
+                        /hbar*dt/ii*Ebind
+!      f_out = 0d0
+      decay_sum_part_m = matmul(decay_via(Ndo_m, :), coul_mat(abs(Ndo_m-Nm_o-1)+1, :, :))
+      decay_out(Ndo_m, :) = -ii*(y*y*decay_via(Ndo_m, :) - ii*decay_m* decay_via(Ndo_m,:) - 2.0d0*decayf_sum + &
+                        shift*decay_via(Ndo_m, :) + ii * gamma * p_via(Ndo_m, :) -&
+                        ((decay_sum_part_m-2.0d0*fdecay_sum) ) )/hbar*dt*Ebind
     end do
+!      write(807, format_V) -real((conjg(Etime(nt_via)*dipole/Ebind)*p_via(Nm_o+1, :) - Etime(nt_via)*dipole/Ebind&
+!                        *conjg(p_via(Nm_o+1, :)))&
+!                        /hbar*dt*ii*Ebind)
+!      write(808, format_V) real(f_out(Nm_o+1,:))
+    t1 = dt*nt_via+tstart_A
     J_THZ_t1 = 0d0
-    J_THZ_t1 = dy*sum(y*y*(p_via(Nm_o, :)+p_via(Nm_o+2, :)))/(2.0d0*pi)
-    kE_out = dt * Etime(nt_via) * exp((0.0d0, 1.0d0)*(dt*nt_via+tstart_A)*freqgrid)  
-    kA_out = dt * Atime(nt_via) * exp((0.0d0, 1.0d0)*(dt*nt_via+tstart_A)*freqgrid) 
+    J_THZ_t1 = dy*sum(y*y*(f_via(Nm_o, :)+f_via(Nm_o+2, :)))/(2.0d0*pi)
+    kE_out = dt * Etime(nt_via) * exp(ii*t1*freqgrid)  
+    kA_out = dt * Atime(nt_via) * exp(ii*t1*freqgrid) 
     pt1 = dy*sum(y*p_via(Nm_o+1, :)*dipole)/(2.0d0*pi)/5d-10   !calculate macroscopic polarization
-    kPfreq_out = dt * pt1 * exp((0.0d0, 1.0d0)*freqgrid*(tstart_A+dt*(nt_via)))
-    kJ_out = dt * J_THZ_t1 * exp((0.0d0, 1.0d0)*freqgrid*(tstart_A+dt*(nt_via)))
+    kPfreq_out = dt * pt1 * exp(ii*freqgrid*t1)
+    kJ_out = dt * J_THZ_t1 * exp(ii*freqgrid*t1)
+    kP1freq_out = dt * (dy*sum(y*y*(p_via(Nm_o, :)+p_via(Nm_o+2, :)))/(2.0d0*pi)) * exp(ii*freqgrid*t1)
   end subroutine RHS
   end module RK_help
--- arguments.f90
+++ arguments.f90
@@ -8,10 +8,10 @@
   !t_end: whole time
 !  integer                                      ::m(2*Nm_o+1) = 0
 !  !m: order of circular harmonics
-  double precision, parameter                  ::ymax = 35.0d0, dy = ymax/dble(Ny) 
+  double precision, parameter                  ::ymax = 15.0d0, dy = ymax/dble(Ny) 
   !ymax: maximum value for y   dy: length of one step for y grid
-  double precision, parameter                  ::sigmat = 0.3d0, tstart = -1d0,&
-                                                 sigmat_A = 0.2d0, tstart_A = -1d0  
+  double precision, parameter                  ::sigmat = 0.3d0, tstart = -3d0,&
+                                                 sigmat_A = 0.05d0, tstart_A = -0.4d0 
 !                                                 sigmat_A = 0.2d0*scale1, tstart_A = -0.5d0*scale1  
   !sigmat: sigma for gaussian pulse(E(t) = exp(-t^2/sigmat^2))(ps)   tstart: starting point for
   !time (ps) 
@@ -19,11 +19,12 @@
   !physical parameters
   double precision, parameter                  ::hbar = 0.6582119514d0, e = 1.60217662d-19  
   !physical parameters  unit of hbar: meV * ps
-  double precision, parameter                  ::Ebind = 4.18d0, gamma = 1.56d0, Eg = 1.50d3
+  double precision, parameter                  ::Ebind = 4.18d0, gamma = 1d0/Ebind, Eg = 1.50d3, decay_m =5.0d0*gamma
   !Ebind: binding energy(meV)   gamma: dephasing factor (meV) Eg: ground state energy
-  double precision, parameter                  ::omega_1s = (0d0)/hbar, &    
-                                                 A_freq_para = (Eg)/2d0/hbar
-!                                                 A_freq_para = (4d0-4d0/9d0)*Ebind/hbar
+  !decay_m: dephasing caused by quantum memory unit: meV
+  double precision, parameter                  ::omega_1s = (0d0)/hbar/2d0
+                                                 !A_freq_para = (Eg-4d0*Ebind+2d0*Ebind+12d0*Ebind*(20d0/50d0 - 0.5d0))/2d0/hbar
+                                                 
 !                                                 A_freq_para = omega_1s
 !                                                dipole: unit: m*e(elementary charge)
   double precision, parameter                  ::dt = t_end/dble(Nt)           
@@ -42,7 +43,7 @@
                                                  W2( Ny ), A2( LDA, Ny )
   character                                    ::YES = 'V',NO = 'N'
 !-------------------end LAPACK-----------------------
-  double precision                             ::E_excit = 1.0d-3, shift = (Eg - omega_1s*hbar)/Ebind,A_excit = 1d6*(Eg/3d0/hbar)/A_freq_para
+  double precision                             ::E_excit = 1.0d-3, shift = (Eg - omega_1s*hbar)/Ebind, A_excit = 0.0d0, A_freq_para = 0.0d0, Ascale = 0.5d0
   !E_excit: excitation level(unit: binding energy)  shift: shift caused by rotation frame 
   !(unit: binding energy)
   !A_excit: unit: V*ps/m
@@ -50,30 +51,32 @@
                                                  Etemp(Ny), Etemp1(Ny), Emax, dipole(Ny) = 5d-10
   !coul_mat: Coulomb matrix(non-symmetric)(unit: binding energy) Et: electrical field 
   !for excitation
-  double precision                             ::y(Ny)=0.0d0, delta_1s = A_freq_para - omega_1s
+  double precision                             ::y(Ny)=0.0d0, delta_1s = 0.0d0
   !y: grid for y
   double precision                             ::y_fine(N_fine) = 0.0d0, dy_fine
   !y_fine: finer grid for removal of singularity   dy_fine: length of one step of finer grid for
   !removal of ringularity  f:density
   complex*16                                   ::p(2*Nm_o+1, Ny) = 0.0d0, ii = (0.0d0, 1.0d0), &
-                                                 f(2*Nm_o+1, Ny) = 0.0d0, coup(Ny),&
-                                                 p_proj(Nt_RWA, Ny), p_proj1(Nt_RWA, Ny), &
-                                                 p_proj2(Nt_RWA, Ny), p_proj11(Nt_RWA, Ny), &
-                                                 p_proj22(Nt_RWA, Ny)                
+                                                 f(2*Nm_o+1, Ny) = 0.0d0, coup(Ny), &
+                                                 decay(2*Nm_o+1, Ny) = 0.0d0, &
+                                                 p_proj(Ny), p_proj1(Ny), &
+                                                 p_proj2(Ny), p_proj11(Ny), &
+                                                 p_proj22(Ny)                
   !p: polarization   f: density		coup: magnetism coupling term
   complex*16                                   ::pt(2*Nm_o+1) = 0.0d0, &
                                                  ft(2*Nm_o+1) = 0.0d0, &
                                                  J_THZ_t = 0.0d0                 
   !pt: macroscopic polarization as a function of time ft: macroscopic density as a function of time
   complex*16                                   ::p_freq(N_freq) = 0.0d0, E_freq(N_freq) = 0.0d0,&
-                                                 A_freq(N_freq) = 0.0d0, J_THZ_freq(N_freq) = 0.0d0   
+                                                 A_freq(N_freq) = 0.0d0, J_THZ_freq(N_freq) = 0.0d0, &
+                                                 p1_freq(N_freq) = 0.0d0   
   !p_freq: Fourier transform of polarization   E_freq: Fourier transform of electrical field
-  double precision                             ::freqgrid(N_freq) = 0.0d0, test(Nphi) = 0.0d0     
+  double precision                             ::freqgrid(N_freq) = 0.0d0, test(Ny) = 0.0d0     
   !freqgrid: frequency grid used for Fourier transform   test: irrelevant with code
   character(80)                                ::list_file, list_file1, list_file2, list_file3, &
                                                  list_file4, list_file5
   !list_file: file name for output
-  character(len=100)                           ::format_V, format_V1, format_V2, format_V3     
+  character(len=100)                           ::format_V, format_V1, format_V2, format_V3, format_V4     
   !format_V: format for output
   contains
 
